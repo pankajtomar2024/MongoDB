19769e1f0f5468f171787a1e7244924a43391a3a
pankaj <pankaj.tomar@impronics.co.za>
2024-06-26 19:21:27 +0530

    fixes

--- Database/connection.js
+++ Database/connection.js
@@ -5,7 +5,7 @@ const password = "7G8qHpCav0W9Q2nB";
 
 mongoose
   .connect(
-    `mongodb+srv://${userName}:${password}@cluster0.dwxs9gt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
+    `mongodb+srv://root:@7EHF8m5uYSd9wq@cluster0.dwxs9gt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
     {
       // useCreateIndex:true,
       useNewUrlParser: true,
